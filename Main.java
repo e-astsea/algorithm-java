3b4301498ca549539cdd12b72b06c74f1bb5b8da
donghae <donghae1999@gmail.com>
2022-07-03 21:58:33 +0900

    그리드 알고리즘

--- Main.java
+++ Main.java
@@ -3,29 +3,46 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
-     int i=0;
-     while(true){
-         int L=sc.nextInt();
-         int P=sc.nextInt();
-         int V=sc.nextInt();
-         if(L==0 && P == 0 && V == 0){
-             break;
-         }
+     int N, M;
+     int result=0;
+     N= sc.nextInt();
+     M= sc.nextInt();
+     int [][]cost = new int[M][2];
 
-         int div=V/P;
-         int per=V%P;
-         int re = 0;
+    for(int i=0; i<M; i++) {
+        cost[i][0] = sc.nextInt();
+        cost[i][1] = sc.nextInt();
+    }
 
-         if(per>L){
-            re = L;
-         }
-         else
-             re = per;
+    int max = cost[0][0];
+    for(int j=1; j<M; j++){
+        if(cost[j][0]<max){
+            max = cost[j][0];
+        }
+    }
 
-         re += div*L;
-         System.out.println("Case"+" "+ ++i +": "+re);
-     }
+    int max_1 = cost[0][1];
+    for(int j=1; j<M; j++){
+        if(cost[j][1]<max_1){
+            max_1 = cost[j][1];
+        }
+    }
 
+    if(max_1*6<max){
+        result += max_1*N;
+    }
+    else {
+        if ((N / 6) > 0) {
+            result += (max * (N / 6));
+            N %= 6;
+        }
+        if (max_1 * N > max) {
+            result += max;
+        } else
+            result += (max_1 * N);
+    }
+
+        System.out.println(result);
     }
 }
 
---
+++ S1049.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class S1049 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N, M;
+        int result=0;
+        N= sc.nextInt();
+        M= sc.nextInt();
+        int [][]cost = new int[M][2];
+
+        for(int i=0; i<M; i++) {
+            cost[i][0] = sc.nextInt();
+            cost[i][1] = sc.nextInt();
+        }
+
+        int max = cost[0][0];
+        for(int j=1; j<M; j++){
+            if(cost[j][0]<max){
+                max = cost[j][0];
+            }
+        }
+
+        int max_1 = cost[0][1];
+        for(int j=1; j<M; j++){
+            if(cost[j][1]<max_1){
+                max_1 = cost[j][1];
+            }
+        }
+
+        if(max_1*6<max){
+            result += max_1*N;
+        }
+        else {
+            if ((N / 6) > 0) {
+                result += (max * (N / 6));
+                N %= 6;
+            }
+            if (max_1 * N > max) {
+                result += max;
+            } else
+                result += (max_1 * N);
+        }
+
+        System.out.println(result);
+    }
+}
+
