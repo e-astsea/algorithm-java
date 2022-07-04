867722603d36b5512108b458b604424a9a05f295
donghae <donghae1999@gmail.com>
2022-07-04 11:00:42 +0900

    그리드 알고리즘

--- B2864.java
+++ B2864.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class B2864 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String A=sc.next();
