96388a494ec27e630d4e673b7d03fb4f9a87b25b
donghae <donghae1999@gmail.com>
2022-07-06 23:02:59 +0900

    스택

---
+++ S10828.java
@@ -0,0 +1,56 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class S10828 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            String str = st.nextToken();
+            if(str.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                stack.push(num);
+            }
+            else if(str.equals("top")) {
+                if(stack.isEmpty()) {
+                    System.out.println("-1");
+                }
+                else
+                    System.out.println(stack.peek());
+            }
+            else if(str.equals("size")) {
+                if(!stack.isEmpty())
+                    System.out.println(stack.size());
+                else
+                    System.out.println("0");
+            }
+            else if(str.equals("empty")) {
+                if(stack.isEmpty())
+                    System.out.println("1");
+                else
+                    System.out.println("0");
+            }
+            else if(str.equals("pop")) {
+                if(stack.isEmpty())
+                    System.out.println("-1");
+                else if(!stack.isEmpty()) {
+                    int a = stack.pop();
+                    System.out.println(a);
+                }
+            }
+
+        }
+
+    }
+
+}
