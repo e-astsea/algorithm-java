5224a1b26a7b5719bec8c1053bed43bead28b778
donghae <donghae1999@gmail.com>
2022-07-07 23:25:23 +0900

    문자열

---
+++ B9093.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B9093 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String sa = br.readLine();
+        int N = Integer.parseInt(sa);
+
+        for(int i=0; i<N; i++) {
+            String s = br.readLine();
+            StringTokenizer st = new StringTokenizer(s, " ", true);
+            while(st.hasMoreTokens()) {
+                StringBuffer sb = new StringBuffer(st.nextToken());
+                String rs = sb.reverse().toString();
+                System.out.print(rs);
+            }
+            System.out.println();
+
+
+        }
+
+
+
+
+
+
+    }
+
+}
