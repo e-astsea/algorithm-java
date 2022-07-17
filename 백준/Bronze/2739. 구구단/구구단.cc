2e6d4c21ffbbac1e9b84b8784fb13d27908e156d
EastSea <86547109+hector-kim@users.noreply.github.com>
2022-07-17 19:36:35 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+
+	int N;
+	
+	cin >> N;
+
+	for (int i = 0; i < 9; i++) {
+		cout << N << " * " << (i + 1) <<" = "<<N*(i+1)<< "\n";
+	}
+   
+
+}
