0b6128d14cb8d5b80109a7a5f4b7d28833fbbd3a
EastSea <86547109+hector-kim@users.noreply.github.com>
2022-07-17 15:59:02 +0900

    [Gold III] Title: 나머지 합, Time: 604 ms, Memory: 121156 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+
+        String N = st.nextToken();
+        String M = st.nextToken();
+
+        str = br.readLine();
+        st=new StringTokenizer(str);
+
+        long []sum = new long[Integer.parseInt(N)];
+        long []count= new long[Integer.parseInt(M)];
+
+
+        sum[0]=Integer.parseInt(st.nextToken());
+        for(int i=1; i<Integer.parseInt(N);i++){
+            sum[i]=sum[i-1]+Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<Integer.parseInt(N);i++){
+            sum[i]%=Integer.parseInt(M);
+        }
+
+        long result=0;
+        for(int i=0; i<Integer.parseInt(N);i++){
+            if(sum[i]==0){
+                result++;
+
+                count[(int)sum[i]]++;
+            }
+            else{
+                count[(int)sum[i]]++;
+            }
+        }
+        for(int i=0; i<Integer.parseInt(M);i++){
+            if(count[i]!=0) {
+                result += ((count[i] * (count[i] - 1)) / 2);
+            }
+        }
+        System.out.println(result);
+
+
+
+
+
+    }
+
+}
