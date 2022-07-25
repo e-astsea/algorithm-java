8022be8f2cc962760c23c8f0d655cb75d78f91bf
EastSea <86547109+hector-kim@users.noreply.github.com>
2022-07-25 10:32:01 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1516 ms, Memory: 58476 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+class pair implements Comparable<pair>{
+    int x;
+    int y;
+    pair(int x, int y){
+
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public int compareTo(pair p){
+        if(this.x>p.x){
+            return 1;
+        }
+        else if(this.x==p.x) {
+            if(this.y>p.y){
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int N = Integer.parseInt(str);
+        ArrayList<pair> al = new ArrayList<>();
+        for(int i=0; i<N; i++){
+            str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            al.add(new pair(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
+        }
+        Collections.sort(al);
+        for(int i=0; i<al.size(); i++){
+            pair p = al.get(i);
+            System.out.println(p.x+" "+p.y);
+        }
+    }
+}
