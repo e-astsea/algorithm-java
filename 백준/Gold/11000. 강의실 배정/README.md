ac5e9051b2ac7caaaa67324bc4cc020172662347
EastSea <86547109+hector-kim@users.noreply.github.com>
2022-07-25 14:19:22 +0900

    [Gold V] Title: 강의실 배정, Time: 1436 ms, Memory: 314068 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/README.md
@@ -0,0 +1,12 @@
+# [Gold V] 강의실 배정 - 11000 
+
+[문제 링크](https://www.acmicpc.net/problem/11000) 
+
+### 성능 요약
+
+메모리: 314068 KB, 시간: 1436 ms
+
+### 분류
+
+자료 구조(data_structures), 그리디 알고리즘(greedy), 우선순위 큐(priority_queue), 정렬(sorting)
+
---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int [][]T = new int[N][2];
+
+        for(int i=0; i<N; i++){
+            T[i][0]=sc.nextInt();
+            T[i][1]=sc.nextInt();
+        }
+
+        Arrays.sort(T, (o1,o2) -> {
+            if(o1[0] == o2[0]){
+                return o1[1] - o2[1];
+            }
+            else
+                return o1[0] - o2[0];
+        });
+
+
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(T[0][1]);
+        for(int i = 1; i < N; i++) {
+            if(pq.peek() <= T[i][0])
+                pq.poll();
+            pq.add(T[i][1]);
+        }
+        System.out.println(pq.size());
+
+    }
+}
+
+
