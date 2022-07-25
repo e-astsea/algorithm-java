bd87aabd1bdffc9c89753cad184e40c9003e51b1
EastSea <86547109+hector-kim@users.noreply.github.com>
2022-07-25 14:20:37 +0900

    [Gold IV] Title: 오큰수, Time: 988 ms, Memory: 148240 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int N = Integer.parseInt(str);
+        str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        int []arr = new int[N];
+        int []ans = new int[N];
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<N; i++){
+            arr[i]=(Integer.parseInt(st.nextToken()));
+        }
+
+        stack.push(0);
+        for(int i=1; i<N; i++){
+            while(!stack.isEmpty() && arr[stack.peek()]<arr[i]){
+                int b =stack.pop();
+                ans[b]=arr[i];
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            int a = stack.pop();
+            ans[a]=-1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int j =0; j<N; j++) {
+            sb.append(ans[j]).append(' ');
+        }
+        System.out.println(sb);
+
+    }
+}
