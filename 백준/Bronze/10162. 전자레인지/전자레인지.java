56c3f6b22f6b0b1d9824b6281b003d4658b3c6a3
김동해 <86547109+hector-kim@users.noreply.github.com>
2022-12-31 12:56:33 +0900

    [Bronze IV] Title: 전자레인지, Time: 248 ms, Memory: 18684 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sec = sc.nextInt();
+        int min = 0;
+        if(sec>=60){
+            min = sec/60;
+            sec %= 60;
+        }
+        int a=0,b=0,c=0;
+
+        if(min>=5){
+            a = (min / 5);
+            min %=5;
+        }
+        if(min>0){
+            b = (min/1);
+            min %=1;
+        }
+        if(sec%10==0){
+            c = sec/10;
+        }
+        else {
+            System.out.println("-1");
+            return;
+        }
+
+        System.out.println(a+" "+b+" "+c);
+    }
+}
