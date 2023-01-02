955f54eb98ac3eaf1eb67718c5b9f3e034f31cab
김동해 <86547109+hector-kim@users.noreply.github.com>
2023-01-03 00:21:40 +0900

    [Silver IV] Title: 동전 0, Time: 216 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int N=sc.nextInt(), K= sc.nextInt();
+            int [] arr = new int[N];
+
+            for(int i=0; i<N; i++){
+                arr[i] = sc.nextInt();
+            }
+            int result =0;
+            for(int j=N-1; j>=0; j--){
+                result += K/arr[j];
+                K %= arr[j];
+            }
+            System.out.println(result);
+        }
+
+}
