eabc9e6651e8e7c7230dfead963e8102a65ba10a
김동해 <86547109+hector-kim@users.noreply.github.com>
2023-01-05 00:09:17 +0900

    Delete B15596.java

--- B15596.java
+++
@@ -1,9 +0,0 @@
-public class B15596 {
-    public static long sum(int[] a){
-        long ans=0;
-        for(int i=0; i<a.length; i++){
-            ans+=a[i];
-        }
-        return ans;
-    }
-}
