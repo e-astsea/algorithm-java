eddbde7013b77b1b0ee23ebeba801d034e28ceae
김동해 <86547109+hector-kim@users.noreply.github.com>
2023-01-05 00:09:23 +0900

    Delete B18108.java

--- B18108.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class B18108 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
-        System.out.println(year - 543);
-    }
-}
