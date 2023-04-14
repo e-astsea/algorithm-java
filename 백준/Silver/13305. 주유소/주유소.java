ddba8ca627a96a8498bec3bd0e0394a663da455c
Ocean <donghae1999@gmail.com>
2023-04-14 23:39:38 +0900

    [Silver III] Title: 주유소, Time: 400 ms, Memory: 36400 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,28 +1,37 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        long []length = new long[N-1];
-        long []cost = new long[N];
-        for(int i=0; i<N-1; i++){
-            length[i]=sc.nextInt();
-        }
-        for(int i=0; i<N; i++){
-            cost[i]=sc.nextInt();
-        }
-        long mincost = cost[0];
-        long oil_cost=0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(bufferedReader.readLine());
 
-        for(int i=0; i<N-1; i++){
-            if(mincost>cost[i]){
-                mincost=cost[i];
+        long[] length = new long[N - 1];
+        long[] cost = new long[N];
+
+        st = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < N - 1; i++) {
+            length[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        st = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        long mincost = cost[0];
+        long oil_cost = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            if (mincost > cost[i]) {
+                mincost = cost[i];
             }
             oil_cost += mincost * length[i];
-
         }
+        
         System.out.println(oil_cost);
     }
 }
