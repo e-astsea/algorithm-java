41f5ee0f98b96e353ab2780027630a3fb10e2950
Ocean <donghae1999@gmail.com>
2023-04-29 20:28:34 +0900

    [Silver I] Title: 안전 영역, Time: 268 ms, Memory: 27200 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,40 +6,36 @@ import java.util.StringTokenizer;
 
 
 public class Main {
-    static int [][] arr = new int[101][101];
-    static int [][] tmp;
-    static int [] dx = {0,1,0,-1};
-    static int [] dy = {1,0,-1,0};
-    static int [][] checked;
+    static int[][] arr = new int[101][101];
+    static int[][] tmp;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] checked;
     static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         String str = br.readLine();
         N = Integer.parseInt(str);
 
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             str = br.readLine();
-            StringTokenizer st = new StringTokenizer(str);
-            for(int j=0; j<N; j++){
-                arr[i][j]=Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(str);
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int max=-1;
-        int cnt =0;
-        for(int m=0; m<101; m++) {
-            tmp=new int[101][101];
-            checked=new int[101][101];
-            cnt=0;
-            for (int a = 0; a < N; a++) {
-                for (int b = 0; b < N; b++) {
-                    if (arr[a][b] <= m) {
-                        tmp[a][b] = 0;
-                    } else {
-                        tmp[a][b] = 1;
-                    }
-                }
-            }
 
+        int max = -1;
+        int cnt = 0;
+
+        for (int waterHeight = 0; waterHeight < 101; waterHeight++) {
+            tmp = new int[101][101];
+            checked = new int[101][101];
+            cnt = 0;
+            checkUnderWater(waterHeight);
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if (tmp[i][j] == 1 && checked[i][j] != 1) {
@@ -47,25 +44,36 @@ public class Main {
                     }
                 }
             }
-            if(max<=cnt)
-                max=cnt;
+            if (max <= cnt)
+                max = cnt;
         }
 
         System.out.println(max);
     }
-    static void dfs(int x, int y){
-        checked[x][y]=1;
 
-        for(int i=0; i<4; i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(nx>=0 && ny>=0 && nx<N && ny<N){
-                if(tmp[nx][ny]==1 && checked[nx][ny]!=1){
-                    dfs(nx,ny);
+    private static void checkUnderWater(int waterHeight) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] <= waterHeight) {
+                    tmp[i][j] = 0;
+                } else {
+                    tmp[i][j] = 1;
                 }
             }
         }
+    }
 
+    static void dfs(int x, int y) {
+        checked[x][y] = 1;
 
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                if (tmp[nx][ny] == 1 && checked[nx][ny] != 1) {
+                    dfs(nx, ny);
+                }
+            }
+        }
     }
 }
