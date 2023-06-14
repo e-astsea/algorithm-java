df55863f9f84becb30602a4f31fea32f23da503d
동해 <donghae1999@gmail.com>
2023-06-15 00:09:17 +0900

    [level 1] Title: 예산, Time: 0.40 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i=0; i<d.length; i++){
+            budget -= d[i];
+            if(budget<0){
+                break;
+            }
+            else{
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
