9af72f481853c5d42c0fc2c4f9f249332eab1906
동해 <donghae1999@gmail.com>
2023-10-18 00:27:40 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,61 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class pair {
-    int x;
-    int y;
-    pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
 public class Main {
+    static class Pair{
+        int x;
+        int y;
+
+        public Pair(final int x, final int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     static int N;
     static int M;
+    static boolean[][] visited;
     static int[][] arr;
-    static int[][] visit;
-    static int[] dx ={0,-1,0,1};
-    static int[] dy ={-1,0,1,0};
-    static Queue<pair> que = new LinkedList<>();
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
+        visited = new boolean[N][M];
         arr = new int[N][M];
-        visit = new int[N][M];
         for (int i = 0; i < N; i++) {
-            str = br.readLine();
-            for(int j=0; j<M; j++) {
-                arr[i][j]=str.charAt(j)-'0';
+            final String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = false;
+                arr[i][j] = str.charAt(j)-'0';
             }
         }
-        bfs(0,0);
 
-        System.out.println(arr[N-1][M-1]);
-        }
-    static void bfs(int x, int y){
-        visit[x][y]=1;
-        que.add(new pair(0,0));
-        while(!que.isEmpty()) {
-            pair point = que.remove();
-            for (int i = 0; i <4; i++) {
-                int nx = point.x+dx[i];
-                int ny = point.y+dy[i];
-                if(nx>=0 && ny>=0 && nx<N && ny<M){
-                    if(arr[nx][ny]==1 && visit[nx][ny]!=1){
-                        visit[nx][ny]=1;
-                        arr[nx][ny]=arr[point.x][point.y]+1;
-                        que.add(new pair(nx,ny));
-                    }
-                }
-
-            }
-        }
+        final int bfs = bfs(new Pair(0, 0));
+        System.out.println(bfs);
     }
 
+    private static int bfs(final Pair pair) {
+        Queue<Pair> que = new LinkedList<>();
+        que.add(pair);
+        visited[pair.x][pair.y] = true;
+        int[][] cal = new int[N][M];
+        cal[0][0] = 1;
+        while(!que.isEmpty()){
+            final Pair poll = que.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = poll.x + dx[i];
+                int ny = poll.y + dy[i];
+                if(nx<0 || ny<0 || nx>=N || ny>=M){
+                    continue;
+                }
+                if(visited[nx][ny] || arr[nx][ny] == 0){
+                    continue;
+                }
+                que.add(new Pair(nx,ny));
+                cal[nx][ny] = cal[poll.x][poll.y] + 1;
+                visited[nx][ny]=true;
+            }
+        }
+        return cal[N-1][M-1];
+    }
 }
