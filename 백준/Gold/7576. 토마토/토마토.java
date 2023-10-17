cdef5ab3ceab085b0404dc3776cbb53e4cb54c2a
동해 <donghae1999@gmail.com>
2023-10-18 00:41:44 +0900

    [Gold V] Title: 토마토, Time: 680 ms, Memory: 125344 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,87 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-// 두개가 한번에 안작동하는 현상, 1이 두개이상일경우..
-// 시작 지점 여러개 어케하나 ?
-class pair{
-    int x;
-    int y;
-    pair(int x, int y){
-        this.x = x;
-        this.y =y;
-    }
-}
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int M;
+
+    static class Pair{
+        int x;
+        int y;
+
+        public Pair(final int x, final int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     static int N;
-    static int[][]arr;
-    static int[][]checked;
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};
-    static Queue<pair> que = new LinkedList<>();
+    static int M;
+
+    static int[][] board;
+    static int[][] tmp;
+    static boolean[][] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
-         M = Integer.parseInt(st.nextToken());
-         N = Integer.parseInt(st.nextToken());
 
-        arr = new int[N+1][M+1];
-        checked =  new int[N+1][M+1];
-        for(int i=0; i<N; i++){
-            str = br.readLine();
-            st = new StringTokenizer(str);
-            for(int j=0; j<M; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(arr[i][j]==1)
-                    que.add(new pair(i,j));
-            }
-        }
-        bfs();
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        board = new int[N][M];
+        visited = new boolean[N][M];
+        Queue<Pair> que = new LinkedList<>();
+        tmp = new int[N][M];
 
-        int check = 0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(arr[i][j]==0){
-                    check=1;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = false;
+                board[i][j] = Integer.parseInt(st.nextToken());
+
+                if(board[i][j] == 1){
+                    que.add(new Pair(i,j));
+                    visited[i][j] = true;
+                    tmp[i][j]=0;
                 }
             }
         }
-        if(check==0 && max!=0)
-            System.out.println(max-1);
-        else if(max==0 && check==0){
-            System.out.println(max);
+
+        bfs(que);
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if(!visited[i][j] && board[i][j] == 0){
+                    System.out.println("-1");
+                    return;
+                }
+                max = Math.max(max,tmp[i][j]);
+            }
         }
-        else
-            System.out.println("-1");
+        System.out.println(max);
     }
-    static int max = 0;
 
-    static void bfs(){
+    private static void bfs(final Queue<Pair> que) {
         while(!que.isEmpty()){
-            pair tmp = que.remove();
-            checked[tmp.x][tmp.y]=1;
-            for(int i=0; i<4; i++){
-                int nx = tmp.x+dx[i];
-                int ny = tmp.y+dy[i];
-
-                if(nx>=0 && ny>=0 && nx<N && ny<M){
-                    if(arr[nx][ny]==0 && checked[nx][ny]!=1){
-                        checked[nx][ny]=1;
-                        arr[nx][ny] = arr[tmp.x][tmp.y]+1;
-                        if(arr[nx][ny]>max){
-                            max = arr[nx][ny];
-                        }
-                        que.add(new pair(nx,ny));
-                    }
+            final Pair poll = que.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i]+poll.x;
+                int ny = dy[i]+poll.y;
+                if(nx<0 || ny<0 || nx>=N || ny>=M){
+                    continue;
                 }
+                if(board[nx][ny] == -1 || visited[nx][ny]){
+                    continue;
+                }
+                visited[nx][ny]=true;
+                tmp[nx][ny] = tmp[poll.x][poll.y]+1;
+                que.add(new Pair(nx,ny));
             }
         }
     }
