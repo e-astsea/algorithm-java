0b48fda45fbeb9ad8822685d8ec4fe0a44b8ce29
동해 <donghae1999@gmail.com>
2023-10-19 00:34:00 +0900

    [Gold IV] Title: 알파벳, Time: 920 ms, Memory: 15252 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,65 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    static char [][]arr= new char [20][20];
-    static int [][]checked = new int [20][20];
-    static int []dx= {-1,0,1,0};
-    static int []dy= {0,1,0,-1};
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,1,-1};
     static int R;
     static int C;
-    static ArrayList<Character> al = new ArrayList<>();
+    static Character[][] arr;
+    static boolean[] visited;
+    static int count =0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<R; i++){
-            str = br.readLine();
-            for(int j=0; j<C; j++){
-                arr[i][j]=str.charAt(j);
+        arr = new Character[R][C];
+        visited = new boolean[26];
+        for (int i = 0; i < R; i++) {
+            final String str = br.readLine();
+            for (int j = 0; j < C; j++) {
+                arr[i][j] = str.charAt(j);
             }
         }
-        checked[0][0]=1;
+
+        visited[arr[0][0]-65]=true;
         dfs(0,0,1);
-        System.out.println(max);
 
+        System.out.println(count);
     }
-    static int max=0;
-    static int cnt=0;
-    static void dfs(int x, int y, int d){
-        al.add(arr[x][y]);
-        checked[x][y]=1;
-        if(max<d){
-            max=d;
+
+    private static void dfs(final int x, final int y, int depth) {
+        count = Math.max(count, depth);
+        for (int i = 0; i < 4; i++) {
+            int nx = x+ dx[i];
+            int ny = y+ dy[i];
+            if(nx<0 || ny<0 || nx>=R || ny>=C)
+                continue;
+            if(visited[arr[nx][ny]-65])
+                continue;
+
+            visited[arr[nx][ny]-65]=true;
+            dfs(nx,ny, depth+1);
+            visited[arr[nx][ny]-65]=false;
         }
-        for(int i=0; i<4; i++){
-
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            cnt=0;
-            if(nx>=0 && ny>=0 && nx<R && ny<C && checked[nx][ny]!=1){
-                for(int j=0; j<al.size();j++){
-                    if(arr[nx][ny]==al.get(j)){
-                        cnt=1;
-                        break;
-                    }
-                }
-                if(cnt==1){
-                    continue;
-                }
-                dfs(nx,ny,d+1);
-            }
-
-        }
-        al.remove(al.size()-1);
-        checked[x][y]=0;
-
     }
 }
