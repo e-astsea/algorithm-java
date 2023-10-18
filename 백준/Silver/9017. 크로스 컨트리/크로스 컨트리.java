4f4e35cc15312500c78f6e5098a7df24ade9fc92
동해 <donghae1999@gmail.com>
2023-10-18 19:59:12 +0900

    [Silver III] Title: 크로스 컨트리, Time: 220 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
@@ -7,42 +7,33 @@ import java.util.*;
 public class Main{
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int T = Integer.parseInt(str);
-
+        int T = Integer.parseInt(br.readLine());
+        
         for(int i=0; i<T; i++){
             ArrayList<Integer> al = new ArrayList<>();
             ArrayList<Integer> index = new ArrayList<>();
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            str = br.readLine();
-            int N = Integer.parseInt(str);
-
-            str = br.readLine();
-            StringTokenizer st = new StringTokenizer(str);
-
-            //입력
             for(int j=0; j<N; j++){
                 al.add(Integer.parseInt(st.nextToken()));
             }
 
-            //6개 미만 팀 제거
             for(int a=0; a<N; a++) {
                 int count = Collections.frequency(al, al.get(a));
                 if(count<6) {
                     index.add(a);
                 }
             }
+
             for(int a=index.size()-1; a>=0; a--){
                 int ind = index.get(a);
                 al.remove(ind);
             }
 
-
-            //5등 먼저 들어오는 팀 five배열통해 first 에 기록 하고 합산점수 arr에 기록
             int []arr = new int[201];
             int []first = new int[201];
             int []five = new int[201];
-
 
             for(int a=0; a<al.size(); a++){
                 five[al.get(a)]++;
@@ -54,8 +45,6 @@ public class Main{
                 }
             }
 
-
-            //최저이면서 같은 성적일경우 first에 있는 거 위주로
             int score=2000;
             int scoretmp=0;
             int team=0;
@@ -65,6 +54,7 @@ public class Main{
                     scoretmp=a;
                     team = a;
                 }
+                
                 else if(arr[a] == score){
                     if (first[a] < first[scoretmp]) {
                         score = arr[a];
@@ -75,7 +65,6 @@ public class Main{
             }
 
             System.out.println(team);
-
         }
     }
 }
