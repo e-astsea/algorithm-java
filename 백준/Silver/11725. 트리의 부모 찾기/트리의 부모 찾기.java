eca9eab52a0826cc324a6d9517f604ff7147e3f9
동해 <donghae1999@gmail.com>
2024-01-14 13:51:07 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1536 ms, Memory: 72968 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,60 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int M;
-    static int N;
-    static int[]checked;
-    static ArrayList<Integer>[] al;
-    static int[]arr;
-    static Queue<Integer> que = new LinkedList<>();
+    public static int[] result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str);
-        N = Integer.parseInt(st.nextToken());
-        al = new ArrayList[N+1];
-        for(int i=0; i<=N;i++){
-            al[i]=new ArrayList<Integer>();
+        int N = Integer.parseInt(br.readLine());
+
+        List<List<Integer>> ls = new ArrayList<>();
+        result = new int[N+1];
+        for (int i = 0; i <= N; i++) {
+            ls.add(new ArrayList<>());
         }
 
-        for(int i=0; i<N-1; i++){
-            str=br.readLine();
-            st = new StringTokenizer(str);
+        for (int i = 0; i < N-1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int first = Integer.parseInt(st.nextToken());
             int second = Integer.parseInt(st.nextToken());
-            al[first].add(second);
-            al[second].add(first);
-        }
-        for(int i=0; i<=N; i++){
-            Collections.sort(al[i]);
-        }
-            arr = new int[N+1];
-            checked = new int[N+1];
-            bfs(1);
 
-            for(int i=2; i<=N; i++){
-                System.out.println(arr[i]);
-            }
+            ls.get(first).add(second);
+            ls.get(second).add(first);
+        }
+
+        result[1] = -1;
+        dfs(ls,1);
+
+        for (int i = 2; i <=N ; i++) {
+            System.out.println(result[i]);
+        }
     }
 
-    static void bfs(int x){
-
-        que.add(x);
-        while(!que.isEmpty()) {
-            int tmp = que.remove();
-            for (int i = 0; i < al[tmp].size(); i++) {
-                int ge = al[tmp].get(i);
-
-                if(arr[ge]==0) {
-                    arr[ge]=tmp;
-                    que.add(ge);
-                }
+    private static void dfs(List<List<Integer>> ls, int num) {
+        for (int i = 0; i < ls.get(num).size(); i++) {
+            if(result[ls.get(num).get(i)]==0) {
+                result[ls.get(num).get(i)] = num;
+            } else{
+                continue;
             }
+           dfs(ls, ls.get(num).get(i));
         }
-
     }
 }
