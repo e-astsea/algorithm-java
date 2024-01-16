6483aad35b57cc4ca095525d85db44de3abc7f1b
동해 <donghae1999@gmail.com>
2024-01-16 15:09:14 +0900

    [Bronze I] Title: 최소공배수, Time: 196 ms, Memory: 14948 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            System.out.println(N * M / gcd(N, M));
+        }
+    }
+
+    private static int gcd(int n, int m) {
+        if(n%m==0){
+            return m;
+        } else {
+            return gcd(m, n % m);
+        }
+    }
+}
