b482d777e9d66eb5f454dff273a2426877f271bb
동해 <donghae1999@gmail.com>
2024-02-19 14:04:13 +0900

    [Silver III] Title: N과 M (7), Time: 636 ms, Memory: 123644 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            List<Integer> ls = new ArrayList<>();
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                ls.add(Integer.parseInt(st.nextToken()));
+            }
+
+            Collections.sort(ls);
+            arr = new boolean[N];
+            result = new int[N];
+            dfs(ls,M,0);
+            System.out.println(sb.toString());
+        }
+        static boolean[] arr;
+        static int[] result;
+    static StringBuilder sb = new StringBuilder();
+        private static void dfs(List<Integer> ls, int max, int depth) {
+            if(depth == max){
+                for (int i = 0; i < max; i++) {
+                    sb.append(result[i]).append(" ");
+                }
+                sb.append("\n");
+                return;
+            }
+            for (int i = 0; i < ls.size(); i++) {
+                result[depth] = ls.get(i);
+//                if(arr[i]){
+//                    continue;
+//                }
+//                arr[i]=true;
+                dfs(ls,max,depth+1);
+//                arr[i]=false;
+            }
+        }
+}
