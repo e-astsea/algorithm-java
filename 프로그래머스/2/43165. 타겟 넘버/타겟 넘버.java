5cee228975e1d111f9bdafb1f116cf1b4e4077f2
동해 <donghae1999@gmail.com>
2024-03-22 17:32:25 +0900

    [level 2] Title: 타겟 넘버, Time: 0.36 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        
+        dfs(numbers,target,0,0);
+        return answer;
+    }
+    static int answer = 0;
+    public void dfs(int[] numbers, int target, int depth, int sum){
+        if(depth == numbers.length){
+            if(sum==target){
+                answer++;
+            }
+            return;
+        }
+        dfs(numbers,target,depth+1, sum+numbers[depth]);
+        
+        dfs(numbers,target,depth+1, sum-numbers[depth]);
+    }
+}
