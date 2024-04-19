ce4578a9e3afe1ea50bca621a20214c999d559e7
동해 <donghae1999@gmail.com>
2024-04-19 16:30:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.66 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 70.4 MB, 시간: 5.80 ms
+메모리: 56.2 MB, 시간: 4.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 17:50:50
+2024년 04월 19일 16:30:38
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,45 +3,49 @@ class Solution {
     class Pair{
         int x;
         int y;
-        public Pair(int x, int y){
+        int depth;
+        public Pair(int x, int y, int depth){
             this.x = x;
             this.y = y;
+            this.depth = depth;
         }
     }
     static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+    static int[] dy = {0,0,1,-1};
     
     public int solution(int[][] maps) {
         int answer = 0;
-        checked = new int[maps.length][maps[0].length];
-        for(int i=0; i<maps.length; i++){
-            for(int j=0; j<maps[0].length; j++){
-                checked[i][j]=-1;
+        return bfs(maps);
+    }
+    public int bfs(int[][] maps){
+        Queue<Pair> q = new ArrayDeque<>();
+        int[][] checked = new int[maps.length][maps[0].length];
+        
+        q.add(new Pair(0,0,1));
+        checked[0][0]=1;
+        
+        while(!q.isEmpty()){
+            Pair now = q.poll();
+            for(int i=0; i<4; i++){
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+                if(nx<0 || ny<0 || nx>=maps.length || ny>=maps[0].length){
+                    continue;
+                }
+                if(maps[nx][ny]==0){
+                    continue;
+                }
+                if(checked[nx][ny]>0){
+                    continue;
+                }
+                checked[nx][ny]=now.depth+1;
+                q.add(new Pair(nx,ny,now.depth+1));
             }
         }
-        bfs(new Pair(0,0), maps);
+        if(checked[maps.length-1][maps[0].length-1] == 0){
+            return -1;
+        } else{
         return checked[maps.length-1][maps[0].length-1];
-    }
-    static int[][] checked;
-    public void bfs(Pair start, int[][] maps){
-        Queue<Pair> que = new ArrayDeque<>();
-        que.add(start);
-        checked[start.x][start.y] = 1;
-        while(!que.isEmpty()){
-            Pair p = que.poll();
-            for(int i=0; i<4; i++){
-                int nx = p.x+dx[i];
-                int ny = p.y+dy[i];
-                
-                if(nx<0 || ny<0|| nx>=maps.length || ny>=maps[0].length){
-                    continue;
-                }
-                if(maps[nx][ny]==0 || (checked[nx][ny]!=-1 && checked[nx][ny] <= checked[p.x][p.y]+1)){
-                    continue;
-                }
-                checked[nx][ny]=checked[p.x][p.y]+1;
-                que.add(new Pair(nx,ny));
-            }
         }
     }
 }
