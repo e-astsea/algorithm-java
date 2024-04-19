bd3452a19891b0e37242a69d29f89044365e2a5c
동해 <donghae1999@gmail.com>
2024-04-19 16:09:27 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,27 +3,38 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] checked = new boolean[computers.length];
+        List<List<Integer>> ls = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            ls.add(new ArrayList<>());
+        }
         
         for(int i=0; i<computers.length; i++){
+            for(int j=0; j<computers[i].length; j++){
+                if(computers[i][j]==1){
+                    ls.get(i).add(j); 
+                }
+            }
+        }
+        boolean[] checked = new boolean[n];
+        
+        for(int i=0; i<n; i++){
             if(checked[i]){
                 continue;
             }
+            dfs(ls,checked,i);
             answer++;
-            checked[i]=true;
-            dfs(i,computers, checked);
         }
-        
         return answer;
     }
     
-    public void dfs(int start, int[][] computers, boolean[] checked){
-        for(int i=0; i<computers[start].length; i++){
-            if(computers[start][i]==0 || checked[i]){
+    public void dfs(List<List<Integer>> ls, boolean[] checked,int start){
+        
+        for(int i=0; i<ls.get(start).size(); i++){
+            if(checked[ls.get(start).get(i)]){
                 continue;
             }
-            checked[i]=true;
-            dfs(i,computers,checked);
+            checked[ls.get(start).get(i)] = true;
+            dfs(ls,checked,ls.get(start).get(i));
         }
     }
 }
